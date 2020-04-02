001a8ae77328b1ba88cabdf33743419e1fae480d
miahmdrubel <miahmdrubel384@gmail.com>
2020-04-02 13:38:53 +0600

    Add files via upload
    Print Numbers in Decending Order from 1000 to 1.After each number one tab is used and after 5 numbers a new line is entered.

---
+++ Problem3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+     cout << "Print Numbers in Decending order:->\n";
+     for(int i = 1000; i >=1; i--){
+
+          if(i%5==0){
+            cout<<endl;
+          }
+          cout << i <<"\t";
+
+     }
+
+
+
+
+
+
+    return 0;
+}
